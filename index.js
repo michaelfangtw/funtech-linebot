5e2486536ccd51602466365ca12f84609735b73a
fij <fij@pchome.com.tw>
2019-12-26 23:49:38 +0800

    v1.3.9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var linebot = require('linebot');
 var express = require('express');
 var http = require('http');
+var https = require('https');
 var fs = require("fs");
+var cheerio=require('cheerio');//html parser
 
 var bot = linebot({
 channelId: '1653670422',
@@ -9,9 +11,13 @@ channelSecret: '2f17a0275f1f57e1d7037f57d529de37',
 channelAccessToken: 'DYMu02TejlJ1CAfkQ4mH8vmNXSato4azQvzyUA1DU8t8uWlnp2kxezvdZhIOh8Y6gb0x1gIkNz6FkcEWvT+VGAQZsUSbqzXTKvGQR0WavMRJolZ2jRLPELKFHLz1PNB4CWv/BWyAxj4dWKl4m0y84gdB04t89/1O/w1cDnyilFU='}
 );
 
-  var timer;
+  var timerPM;
+  var timerUSD
   var pm = [];
+  var usd;//美金
+  var usdTime;
   _getJSON();
+  _getUSD();
   
   _bot();
   const app = express();
@@ -41,18 +47,15 @@ channelAccessToken: 'DYMu02TejlJ1CAfkQ4mH8vmNXSato4azQvzyUA1DU8t8uWlnp2kxezvdZhI
           }
         }
 
-        if (msg.indexOf('PM2.5') != -1) {
-          pm.forEach(function(e, i) {
-            if (msg.indexOf(e.SiteName) != -1) {
-              lineMsg=JSON.stringify(e);
-              replyMsg = e.SiteName + ' '+e.County+'的 PM2.5 數值為 ' +e['PM2.5']+'\r\n空氣品質:'+e.Status+ '\r\n更新時間:'+e.PublishTime;
-            }
-          });
-          if (replyMsg == '') {
-            replyMsg = '請輸入正確的地點';
-          }
+        if (msg.indexOf('美金') != -1) {          
+            replyMsg = '美金即期匯率:'+usd+ ' 更新時間:'+usdTime;
         }
         
+        if (replyMsg == '') {
+          replyMsg = '請輸入正確的地點';
+        }
+      
+
         if (replyMsg == '') {
           replyMsg = '不知道「'+msg+'」是什麼意思 :p';
         }
@@ -70,23 +73,16 @@ channelAccessToken: 'DYMu02TejlJ1CAfkQ4mH8vmNXSato4azQvzyUA1DU8t8uWlnp2kxezvdZhI
   function _getJSON() {    
     var url='http://opendata.epa.gov.tw/api/v1/AQI?%24skip=0&%24top=1000&%24format=json';
     console.log(url); 
-   var body='';
+    var body='';
     var req = http.get(url,function(res) {
       //console.log("statusCode: ", res.statusCode);
-      //console.log("headers: ", res.headers);
-    
+      //console.log("headers: ", res.headers);    
       res.on('data', function (chunk) {
         body += chunk;
     });
 
-      res.on('end', function(){
-        // 將 JSON parse 成物件
-        //console.log("==response.body ==============================================");
-        //console.log(body);
-        //console.log("==response.body ==============================================");
-        var data = JSON.parse(body);
-        //console.log('data=')
-        //console.log(data); // 可開啟這行在 Command Line 觀看 data 內容
+      res.on('end', function(){              
+        var data = JSON.parse(body);       
         var count=0;
         var lastTime='';
         if (Array.isArray(data)){
@@ -94,18 +90,14 @@ channelAccessToken: 'DYMu02TejlJ1CAfkQ4mH8vmNXSato4azQvzyUA1DU8t8uWlnp2kxezvdZhI
               count=i;
               //console.log('i='+i);
               pm[i] = [];
-              // pm[i][0] = e.SiteName; //大里
-              // pm[i][1] = e.County; //臺中市
-              // pm[i][2] = e['PM2.5'] * 1; //18
-              // pm[i][3] = e.Pollutant; //細懸浮微粒
-              // pm[i][4] = e.PublishTime; //2019-12-26 22:00
-              // pm[i][5] = e.Status; //Status
+              // e.SiteName; //大里
+              // e.County; //臺中市
+              // e['PM2.5'] * 1; //18
+              // e.Pollutant; //細懸浮微粒
+              // e.PublishTime; //2019-12-26 22:00
+              // e.Status; //Status
               pm[i]=e;
               lastTime=e.PublishTime;
-
-              //console.log('=============');
-              //console.log(e);
-              //console.log('=============');
             }); 
             console.log("PM2.5資料筆數:"+count);
             console.log("最後更新時間:"+lastTime);
@@ -124,7 +116,37 @@ channelAccessToken: 'DYMu02TejlJ1CAfkQ4mH8vmNXSato4azQvzyUA1DU8t8uWlnp2kxezvdZhI
     });
 
     
-    clearTimeout(timer);    
-    timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
+    clearTimeout(timerPM);    
+    timerPM = setInterval(_getJSON, 1800*1000); //每半小時抓取一次新資料
   }
 
+  
+  function _getUSD() {    
+    var url='https://rate.bot.com.tw/xrt?Lang=zh-TW';
+    console.log(url); 
+    var body='';
+    var req = https.get(url,function(res) {      
+      res.on('data', function (chunk) {
+        body += chunk;
+    });
+
+      res.on('end', function(){
+        //console.log(body);
+          var $ = cheerio.load(body);
+          var target = $(".rate-content-sight.text-right.print_hide");
+          console.log(target[1].children[0].data);
+          usd = target[1].children[0].data;
+          
+          var targetTime = $('.time');          
+          console.log(targetTime[0].children[0].data);
+          usdTime = targetTime;          
+      });
+      req.on('error', function(e) {
+        console.error(e);
+      });
+    });
+
+    
+    clearTimeout(timerUSD);    
+    timerUSD = setInterval(_getUSD, 1800*1000); //每半小時抓取一次新資料
+  }
