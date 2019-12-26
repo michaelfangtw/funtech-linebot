729a04c1b1ec9fd2536b452f00aa5d2713af4cbc
fij <fij@pchome.com.tw>
2019-12-26 22:27:16 +0800

    v1.3

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ channelAccessToken: 'DYMu02TejlJ1CAfkQ4mH8vmNXSato4azQvzyUA1DU8t8uWlnp2kxezvdZhI
     console.log(url); 
    var body='';
     var req = http.get(url,function(res) {
-      console.log("statusCode: ", res.statusCode);
-      console.log("headers: ", res.headers);
+      //console.log("statusCode: ", res.statusCode);
+      //console.log("headers: ", res.headers);
     
       res.on('data', function (chunk) {
         body += chunk;
@@ -67,21 +67,25 @@ channelAccessToken: 'DYMu02TejlJ1CAfkQ4mH8vmNXSato4azQvzyUA1DU8t8uWlnp2kxezvdZhI
 
       res.on('end', function(){
         // 將 JSON parse 成物件
-        console.log("==response.body ==============================================");
-        console.log(body);
-        console.log("==response.body ==============================================");
+        //console.log("==response.body ==============================================");
+        //console.log(body);
+        //console.log("==response.body ==============================================");
         var data = JSON.parse(body);
-        console.log('data=')
+        //console.log('data=')
         //console.log(data); // 可開啟這行在 Command Line 觀看 data 內容
         if (Array.isArray(data)){
             data.forEach(function(e, i) {
+              console.log('i='+i);
               pm[i] = [];
               pm[i][0] = e.SiteName; //高雄(左營)
               pm[i][1] = e.County; //高雄市
               pm[i][2] = e.AQI * 1; //69
               pm[i][3] = e.Pollutant* 1; //細懸浮微粒
               pm[i][4] = e.Status; //Status
-            });
+              console.log('=============');
+              console.log(e);
+              console.log('=============');
+            });           
         }
         //fs.writeFile( 'save.json', JSON.stringify( data ), 'utf8');
         fs.writeFile('pm25.json', JSON.stringify(data),function(err){
