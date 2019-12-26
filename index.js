20bd12bde158f3de8091f422bab8ec19e7974466
fij <fij@pchome.com.tw>
2019-12-26 23:04:19 +0800

    v1.3.8

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ channelAccessToken: 'DYMu02TejlJ1CAfkQ4mH8vmNXSato4azQvzyUA1DU8t8uWlnp2kxezvdZhI
           pm.forEach(function(e, i) {
             if (msg.indexOf(e.SiteName) != -1) {
               lineMsg=JSON.stringify(e);
-              replyMsg = e.SiteName + ' '+e.County+'的 PM2.5 數值為 ' +e['PM2.5']+' 空氣品質:'+e.Status+ '更新時間:'+e.PublishTime;
+              replyMsg = e.SiteName + ' '+e.County+'的 PM2.5 數值為 ' +e['PM2.5']+'\r\n空氣品質:'+e.Status+ '\r\n更新時間:'+e.PublishTime;
             }
           });
           if (replyMsg == '') {
